86fea4c7df067bc7443536da4ce8c8982cfa0586
medukr <andrii.demydyuk@gmail.com>
2018-02-08 11:14:39 +0200

    Modify view: 'cart-modal'

--- views/cart/cart-modal.php
+++ views/cart/cart-modal.php
@@ -8,12 +8,32 @@
                     <th>Кол-во</th>
                     <th>Цена</th>
                     <th>
-                        <button type="button" class="close" aria-label="Close">
+                        <!--<button type="button" class="close" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
-                        </button>
+                        </button>-->
+                        <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
                     </th>
                 </tr>
             </thead>
+            <tbody>
+            <?php foreach ($session['cart'] as $id => $item): ?>
+                <tr>
+                    <td><?= $item['img']?></td>
+                    <td><?= $item['name']?></td>
+                    <td><?= $item['qty']?></td>
+                    <td><?= $item['price']?></td>
+                    <td><span class="glyphicon glyphicon-remove text-danger del-item" aria-hidden="true"></span></td>
+                </tr>
+            <?php endforeach; ?>
+                <tr>
+                    <td colspan="4">Итого:</td>
+                    <td><?= $session['cart.qty']?></td>
+                </tr>
+                <tr>
+                    <td colspan="4">На сумму:</td>
+                    <td><?= $session['cart.sum']?></td>
+                </tr>
+            </tbody>
         </div>
     </div>
 <?php else: ?>

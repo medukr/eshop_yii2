658bc6300728f7b68fd8abb7b096b543576fd6b6
medukr <andrii.demydyuk@gmail.com>
2018-03-06 12:51:15 +0200

    file manager

--- modules/admin/views/product/_form.php
+++ modules/admin/views/product/_form.php
@@ -2,6 +2,10 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use mihaildev\ckeditor\CKEditor;
+use mihaildev\elfinder\ElFinder;
+
+mihaildev\elfinder\Assets::noConflict($this);
 
 /* @var $this yii\web\View */
 /* @var $model app\modules\admin\models\Product */
@@ -23,7 +27,24 @@ use yii\widgets\ActiveForm;
     </div>
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
+<!--    --><?//= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
+
+<!--    --><?php //echo $form->field($model, 'content')->widget(CKEditor::className(),[
+//        'editorOptions' => [
+//            'preset' => 'full', //разработанны стандартные настройки basic, standard, full данную возможность не обязательно использовать
+//            'inline' => false, //по умолчанию false
+//            'language' => Yii::$app->language,
+//            'editorOptions' => ElFinder::ckeditorOptions('elfinder',[]),
+//        ],
+//    ]); ?>
+
+    <?php echo $form->field($model, 'content')->widget(CKEditor::className(),[
+
+            'editorOptions' => ElFinder::ckeditorOptions('elfinder',[
+                'language' => Yii::$app->language,
+            ]),
+
+    ]); ?>
 
     <?= $form->field($model, 'price')->textInput() ?>
 
@@ -33,11 +54,11 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'img')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'hit')->dropDownList([ '0', '1', ], ['prompt' => '']) ?>
+    <?= $form->field($model, 'hit')->checkbox([ '0', '1', ]) ?>
 
-    <?= $form->field($model, 'new')->dropDownList([ '0', '1', ], ['prompt' => '']) ?>
+    <?= $form->field($model, 'new')->checkbox([ '0', '1', ]) ?>
 
-    <?= $form->field($model, 'sale')->dropDownList([ '0', '1', ], ['prompt' => '']) ?>
+    <?= $form->field($model, 'sale')->checkbox([ '0', '1', ]) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
